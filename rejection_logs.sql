c35dc00f762ad04669de6f9da789540e106c7b50
Balita, Christian Neil C. <chankuyinama@gmail.com>
2025-12-18 00:45:12 +0800

    Group 1 - Created a table named "rejection_logs"
    Added value dumps inside rejection_logs

---
+++ rejection_logs.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 17, 2025 at 04:31 PM
+-- Server version: 9.1.0
+-- PHP Version: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sesdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rejection_logs`
+--
+
+DROP TABLE IF EXISTS `rejection_logs`;
+CREATE TABLE IF NOT EXISTS `rejection_logs` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `pending_id` int NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `reason` text NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_pending_id` (`pending_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
