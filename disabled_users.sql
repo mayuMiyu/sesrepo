acb26befb475d406262247a7be31cdaacad8f586
Balita, Christian Neil C. <chankuyinama@gmail.com>
2025-12-18 00:54:47 +0800

    Group 1 - Created a table named "disabled_users"
    Inserted values for disabled users

---
+++ disabled_users.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 17, 2025 at 04:31 PM
+-- Server version: 9.1.0
+-- PHP Version: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sesdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `disabled_users`
+--
+
+DROP TABLE IF EXISTS `disabled_users`;
+CREATE TABLE IF NOT EXISTS `disabled_users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `lrn` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  `disabled_at` datetime NOT NULL,
+  `reason` text,
+  PRIMARY KEY (`id`),
+  KEY `fk_disabled_user` (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
